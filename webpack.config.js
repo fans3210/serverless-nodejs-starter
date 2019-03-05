c2270990d6ed1f1240a7337e6e3631b25fa4b0c4
Jay <jvelayud@gmail.com>
2019-03-05 14:44:38 -0500

    Working on individual packing

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 const slsw = require("serverless-webpack");
-const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: slsw.lib.entries,
   target: "node",
   // Generate sourcemaps for proper error messages
-  devtool: 'source-map',
+  devtool: "source-map",
   // Since 'aws-sdk' is not compatible with webpack,
-  // we exclude all node dependencies
-  externals: [nodeExternals()],
+  externals: [{ 'aws-sdk': 'aws-sdk' }],
   mode: slsw.lib.webpack.isLocal ? "development" : "production",
   optimization: {
     // We do not want to minimize our code.
