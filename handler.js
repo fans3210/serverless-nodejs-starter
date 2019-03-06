ac19ce5670976e57f285aaa6add80fa09143c6ef
Jay <jvelayud@gmail.com>
2019-03-05 19:37:53 -0500

    Working on packing

--- handler.js
+++ handler.js
@@ -1,17 +1,22 @@
 import AWS from "aws-sdk";
 
 export const hello = async (event, context) => {
-  AWS.config.update({region:'us-east-1'});
+  console.log(a.b.c);
+  AWS.config.update({ region: "us-east-1" });
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
+      message: `Go Serverless v1.0! ${await message({
+        time: 1,
+        copy: "Your function executed successfully!"
+      })}`
+    })
   };
 };
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+const message = ({ time, ...rest }) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve(`${rest.copy} (with a delay)`);
+    }, time * 1000)
+  );
